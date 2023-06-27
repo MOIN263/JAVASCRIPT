c6e339b763c7cd61970ba20d978727aada27c8c8
MOIN263 <smoinullah1@gmail.com>
2023-06-27 21:40:46 +0530

    datatypes

---
+++ DatatypesJs/Primitive/1numbers.js
@@ -0,0 +1,18 @@
+
+/*Numbers
+Numbers are integers and decimal values which can do all the arithmetic operations.
+ Let's see some examples of Numbers.*/
+
+//  javascript is a dynamic language so we need not need to specify the datatype
+
+// declaring a number datatype
+// numbers are of two types integers and float 
+
+let a = 40 ;
+const gravity = 9.80;
+
+console.log(a);
+console.log(gravity);
+// to check the datatype we have a builtin fuction called 'typeof' 
+console.log(typeof(a));
+console.log(typeof(gravity));
