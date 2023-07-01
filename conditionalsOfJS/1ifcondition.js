b6b7a8d8cd3f1905cd0b3fb966a9275914421c96
MOIN263 <smoinullah1@gmail.com>
2023-07-01 23:39:57 +0530

    if condition in Js

---
+++ conditionalsOfJS/1ifcondition.js
@@ -0,0 +1,29 @@
+
+// IF CONDITION
+// In JavaScript and other programming languages the key word if is to used check if a condition is true and to execute the block code.
+
+// syntax
+
+/* if (condition) {
+    //this part of code runs for truthy condition
+} */
+
+// example :
+
+// write a block of code to check whether the number is positive
+
+let num = 4
+if (num > 0) {
+    console.log('the number is positive')
+}
+
+// the above code will run only if the condition is true if the condition is false itwill not run
+// for example 
+
+ let lightison = true 
+ if (lightison) { 
+    console.log('switch of the light')
+ }
+
+//  if the above code was false then it would not run
+
