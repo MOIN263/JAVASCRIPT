dcad08af90b98e5e80dc6ddb30e09ee63961f436
MOIN263 <smoinullah1@gmail.com>
2023-07-01 23:37:23 +0530

    Array Methods

---
+++ DatatypesJs/nonPrimitive/4Arraymethods2.js
@@ -0,0 +1,9 @@
+
+// Splice method in array
+// Splice: It takes three parameters:Starting position, number of times to be removed and number of items to be added.
+
+const number = [1,2,3,4,5]
+number.splice()
+console.log(number); 
+// since no arguments were passed all the elements will be logged
+
