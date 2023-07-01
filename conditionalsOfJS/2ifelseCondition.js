1e815d1caaba01b26ba1e90feb9b97e5bb7f25a9
MOIN263 <smoinullah1@gmail.com>
2023-07-01 23:41:08 +0530

    if else in Js

---
+++ conditionalsOfJS/2ifelseCondition.js
@@ -0,0 +1,25 @@
+
+// If Else
+// If condition is true the first block will be executed, if not the else condition will be executed.
+
+// // syntax
+// if (condition) {
+//   // this part of code runs for truthy condition
+// } else {
+//   // this part of code runs for false condition
+// }
+let num = 3
+if (num > 0) {
+  console.log(`${num} is a positive number`)
+} else {
+  console.log(`${num} is a negative number`)
+}
+//  3 is a positive number
+
+num = -3
+if (num > 0) {
+  console.log(`${num} is a positive number`)
+} else {
+  console.log(`${num} is a negative number`)
+}
+//  -3 is a negative number
