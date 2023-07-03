2c7415933ba8480a4deec07d1aad14c4456c6ae9
MOIN263 <smoinullah1@gmail.com>
2023-07-03 21:29:14 +0530

    Ternary operators

---
+++ conditionalsOfJS/4ternaryopts.js
@@ -0,0 +1,19 @@
+// Ternary Operators
+// Another way to write conditionals is using ternary operators. We have covered this in other sections, but we should also mention it here.
+
+let isRaining = true
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
+
+// Write a program to check if a given number is even or odd using a ternary operator
+
+let number = -3
+number > 0 
+? console.log('it is a positive number')
+: console.log('it is a negetive number ')
+
+/*Write a program that takes two numbers as input and finds the maximum using a ternary operator.
+
+Create a program that takes a person's age as input and determines whether they are eligible to vote using a ternary operator.*/
+
