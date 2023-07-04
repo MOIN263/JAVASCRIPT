0cb447247c8e238f9e53717e91ae76bbdd16349b
MOIN263 <smoinullah1@gmail.com>
2023-07-04 20:18:22 +0530

    Do -While loop

---
+++ LOOPSinJS/3dowhile.js
@@ -0,0 +1,30 @@
+// do while loop
+
+let i = 0
+do {
+  console.log(i)
+  i++
+} while (i <= 5)
+
+// 0 1 2 3 4 5
+
+// generally the do while loop is used very less but when the while loop is used and it doesnot satisfy the condition then to print some thing out of it we  use do while loop let me explain it by an example
+
+// In the above example where we are printing the numbers from zero to five if in the starting only it doesnot satisfy the condition like i = 6 
+// then the do while is helpful
+
+let j = 6
+
+do {
+    console.log(j)
+    j++
+  } while (j <= 5)
+
+// we 6 as output
+
+// printing number from 10 to 1 using do while loop
+let k = 10
+do {
+    console.log(k)
+    k--
+}while (k>=1)
